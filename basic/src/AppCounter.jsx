ffd97c302290d3010e421c34aa80854599f396fa
Jin <newmacbookpro@saeloun-ui-MacBookPro.local>
2023-11-22 00:54:45 +0900

    count 이벤트

---
+++ basic/src/AppCounter.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import './App.scss';
+import Counter from './components/Counter';
+
+export default function AppCounter(){
+    const [count, setCount] = useState(0);
+    const handleClick = () => setCount((prev) => prev + 1);
+    return (
+    <div className="">
+        <div className="container">
+            <div className="banner">
+                Total Count : {count} {count > 10 ? '🔥' : '❄️'}
+            </div>
+            <div className="counters">
+                <Counter total={count} onClick={handleClick}/>
+                <Counter total={count} onClick={handleClick}/>
+            </div>
+        </div>
+    </div>
+    );
+}   
---
+++ basic/src/components/Counter.jsx
@@ -0,0 +1,24 @@
+
+import React, { useState } from "react";
+
+export default function Counter({total, onClick}){
+    // let num = 0;
+    const [count, setCount] = useState(0);
+    return (
+       <div className="counter">
+        <span className="number">
+            {count} <span className="total">/{total}</span>
+        </span>
+        <button className="button" onClick={() => {
+            // num++; 자바스크립트 작동되지 않음
+            // count++; 
+            // console.log(num);
+            // setCount(count + 1);
+            setCount((prev) => prev + 1);
+            onClick();
+
+        }}>Add +</button>
+       </div>
+    );
+}
+ 
