a99f31ed96d2cde6a9cd98b87a0004e49916a239
hyejin <hyejinkim.dev@gmail.com>
2023-12-04 21:35:16 +0900

    Immer 사용

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function AppMentor() {
   const [person, setPerson] = useState(initialPerson);
-  const handleUpdate = () => {
+  const handleAdd = () => {
     const name = prompt(`멘토의 이름은?`);
     const title = prompt(`멘토의 직함은?`);
     setPerson((person) => ({
@@ -17,20 +17,7 @@ export default function AppMentor() {
       mentors: person.mentors.filter((m) => m.name !== name)
     }));
   };
-  const handleAdd = () => {
-    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        if (mentor.name === prev) {
-          return { ...mentor, name: current };
-        }
-        return mentor;
-      })
-    }));
-  };
-  const handleChange = () => {
+  const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
     setPerson((person) => ({
@@ -44,6 +31,7 @@ export default function AppMentor() {
     }));
     // 전체 객체를 업데이트 해줘야함, 불변성: 함수형 프로그래밍의 특징
   };
+
   return (
     <div>
       <h1>
@@ -58,9 +46,8 @@ export default function AppMentor() {
         ))}
       </ul>
       <button onClick={handleAdd}>멘토 추가하기</button>
-      <button onClick={handleChange}>멘토의 이름을 바꾸기</button>
+      <button onClick={handleUpdate}>멘토의 이름을 바꾸기</button>
       <button onClick={handleDelete}>멘토 삭제하기</button>
-      <button onClick={handleUpdate}>멘토 추가하기</button>
     </div>
   );
 }
--- basic/src/reducer/person-reducer.js
+++ basic/src/reducer/person-reducer.js
@@ -1,4 +1,4 @@
-export default personReducer(person, action) {
+export default function personReducer(person, action) {
   switch (action.type) {
     case "updated": {
       const { prev, current } = action;
@@ -13,6 +13,22 @@ export default personReducer(person, action) {
           return mentor;
         })
       };
+    }
+    case "added": {
+      const { name, title } = action;
+      return {
+        ...person,
+        mentors: [...person.mentors, { name, title }]
+      };
+    }
+    case "deleted": {
+      return {
+        ...person,
+        mentors: person.mentors.filter((mentor) => mentor.name !== action.name)
+      };
+    }
+    default: {
+      throw Error(`알수없는 액션 타입이다: ${action.type}`);
     }
   }
 }
