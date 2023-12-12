6bc3f62e0842d9b0f993209f81c515dba7350d09
hyejin <hyejinkim.dev@gmail.com>
2023-12-12 23:30:02 +0900

    styled component

--- basic/src/basic/App.js
+++ basic/src/basic/App.js
@@ -1,18 +1,36 @@
 import "./App.scss";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
-// import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-// const Container = styled.div`
-//   display: flex;
-// `;
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `};
+`;
 
 function App() {
   return (
     <>
       <Button1 />
       <Button2 />
-      {/* <Container></Container> */}
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
     </>
   );
 }
